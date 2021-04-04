125a89c6183ff7412956fb61ee9b598f46093f72
Eero Holmala <holmalae@gmail.com>
2021-04-04 07:11:53 +0300

    add sample project skeleton

---
+++ samples/testSample/src/Main.hx
@@ -0,0 +1,17 @@
+class Main extends hxd.App {
+
+	public static var inst: Main;
+
+	override function init() {
+		
+	}
+
+	override function update(dt: Float) {
+		
+	}
+
+	static function main() {
+		hxd.Res.initEmbed();
+		inst = new Main();
+	}
+}
