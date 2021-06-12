8301deb20e188e6277f2f71394f328db9310ae21
Eero Holmala <holmalae@gmail.com>
2021-06-12 21:51:31 +0300

    add dotnet json endpoint

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -1,15 +1,39 @@
-using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-await using var app = builder.Build();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
+});
+
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 }
 
-app.MapGet("/", (Func<string>)(() => "Hello World!"));
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.UseHttpLogging();
 
-await app.RunAsync();
+app.MapGet("/", async httpContext =>
+{
+    
+    await httpContext.Response.WriteAsJsonAsync(new {
+        name = "test"
+    });
+    // .WriteAsync("Hello World!");
+});
+
+
+app.Run();
+
